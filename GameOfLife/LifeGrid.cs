a81fe97d580875e88a3d04b4357e04f7a74a388a
Shane P <sepobanz@gmail.com>
2018-08-18 20:45:14 -0500

    Add project files.

---
+++ GameOfLife/LifeGrid.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class LifeGrid
+    {
+        int[,] gen;
+        int[,] lastGen;
+        int genCount;
+        int width;
+        int height;
+
+        public int GenCount
+        {
+            get { return genCount; }
+        }
+
+        public LifeGrid(int[,] newGrid)
+        {
+            gen = (int[,])newGrid.Clone();
+
+            genCount = 1;
+
+            width = gen.GetLength(1);
+            height = gen.GetLength(0);
+
+            lastGen = new int[height, width];
+        }
+
+        private int Neighbors(int x, int y)
+        {
+            int count = 0;
+
+            // Check for x - 1, y - 1
+            if (x > 0 && y > 0)
+            {
+                if (gen [y - 1, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x, y - 1
+            if (y > 0)
+            {
+                if (gen [y - 1, x] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y - 1
+            if (x < width - 1 && y > 0)
+            {
+                if (gen [y - 1, x + 1] == 1)
+                    count++;
+            }
+
+            // Check for x - 1, y
+            if (x > 0)
+            {
+                if (gen [y, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y
+            if (x < width - 1)
+            {
+                if (gen [y, x + 1] == 1)
+                    count++;
+            }
+
+            // Check for x - 1, y + 1
+            if (x > 0 && y < height - 1)
+            {
+                if (gen [y + 1, x - 1] == 1)
+                    count++;
+            }
+
+            // Check for x, y + 1
+            if (y < height - 1)
+            {
+                if (gen [y + 1, x] == 1)
+                    count++;
+            }
+
+            // Check for x + 1, y + 1
+            if (x < width - 1 && y < height - 1)
+            {
+                if (gen [y + 1, x + 1] == 1)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public void WriteNeighbors()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                    Console.Write("{0}", Neighbors(x, y));
+                Console.WriteLine();
+            }
+        }
+
+        public void ProcessNextGen()
+        {
+            int[,] nextGen = new int[height, width];
+
+            lastGen = (int[,])gen.Clone();
+
+            genCount++;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (gen [y, x] == 0 && Neighbors(x, y) == 3)
+                        nextGen [y, x] = 1;
+                    if (gen [y, x] == 1 &&
+                           (Neighbors(x, y) == 2 || Neighbors(x, y) == 3))
+                        nextGen [y, x] = 1;
+                }
+            }
+
+            gen = (int[,])nextGen.Clone();
+        }
+
+        public void DrawGen()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                    if (gen [y, x] == 0)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    else if (gen [y, x] == 1)
+                    {
+                        Console.Write("*");
+                    }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public int AliveCells()
+        {
+            int count = 0;
+
+            for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
+                    if (gen [y, x] == 1)
+                        count++;
+
+            return count;
+        }
+    }
+}
---
+++ GameOfLife/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Enter the height: ");
+            int height = int.Parse(Console.ReadLine());
+            Console.Write("Enter the width: ");
+            int width = int.Parse(Console.ReadLine());
+
+            int[,] randomGrid = new int[width, height];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    randomGrid[x, y] = rand.Next(0, 2);
+                }
+            }
+
+            LifeGrid lifeGrid = new LifeGrid(randomGrid);
+
+            Console.WriteLine("Generation 0");
+            lifeGrid.DrawGen();
+
+            while (lifeGrid.AliveCells() > 0)
+            {
+                string response;
+
+                Console.WriteLine("\nGeneration {0}", lifeGrid.GenCount);
+
+                lifeGrid.ProcessNextGen();
+
+                lifeGrid.DrawGen();
+
+                if (lifeGrid.AliveCells() == 0)
+                {
+                    Console.WriteLine("Game over!");
+                }
+                else
+                {
+                    Console.Write("Keep going? (y/n): ");
+
+                    response = Console.ReadLine();
+
+                    if (response.ToLower() == "n")
+                        break;
+                }
+            }
+        }
+    }
+}
